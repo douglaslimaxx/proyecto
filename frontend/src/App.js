77f18a4d72bc2391b72ae9f5dd70a650a9286ceb
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2020-09-28 19:51:33 -0300

    adicionei a utilização de props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,126 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Project from './components/Project';
+
+
+var data = {
+  projects: [
+    {
+      id: 0,
+      name: "projeto1",
+      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+      description: "projeto de livros LGBT",
+      progress: 0,
+      components: []
+    },
+    {
+      id: 1,
+      name: "livros",
+      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+      description: "projeto de livros LGBT",
+      progress: 0,
+      components: []
+    },
+    {
+      id: 2,
+      name: "filmesFantasia",
+      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+      description: "projeto de livros LGBT",
+      progress: 0,
+      components: []
+    },
+    {
+      id: 3,
+      name: "filmesTerror",
+      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+      description: "projeto de livros LGBT",
+      progress: 0,
+      components: []
+    }
+  ],
+
+  search: 'filme'
+}
+
 
 function App() {
+
   return (
     <div className="App">
       <h1>Proyecto</h1>
       <div>
-        <input type="search" />
+        <input type="search" id="searchBar" />
         <button>buscar</button>
       </div>
       <br />
-      <select name="sort" id="sort">
-        <option value="progressoDecrescente">Mais próximo de finalizar</option>
-        <option value="progressoCrescente">Mais longe de finalizar</option>
-        <option value="maiselementos">Com mais elementos</option>
-        <option value="avaliacao">Melhor avaliado</option>
-      </select>
-      <table>
-        <tr>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 1</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>50%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 2</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>75%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 3</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>0%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 4</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>0%</p>
-          </td>
-        </tr>
-        <tr>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 1</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>50%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 2</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>75%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 3</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>0%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 4</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>0%</p>
-          </td>
-        </tr>
-        <tr>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 1</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>50%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 2</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>75%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 3</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>0%</p>
-          </td>
-          <td >
-            <img src="https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg"
-              alt="" />
-            <p><b>Projeto 4</b></p>
-            <p>Descrição...</p>
-            <p>Categoria</p>
-            <p>0%</p>
-          </td>
-        </tr>
-      </table>
+      <div>
+        {
+          data.projects.filter(project => project.name.includes(data.search)).map(project => <Project img={project.img} name={project.name} description={project.description} progress={project.progress} />)
+        }
+
+      </div>
     </div>
   );
 }
