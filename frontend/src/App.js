ee18b917a2d3a57c2de88731133ce8b157fe7bc1
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2020-09-29 11:33:16 -0300

    Mudança em componente adicionada e utilização de props adicionada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,64 +3,74 @@ import './App.css';
 import Project from './components/Project';
 
 
-var data = {
-  projects: [
-    {
-      id: 0,
-      name: "projeto1",
-      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
-      description: "projeto de livros LGBT",
-      progress: 0,
-      components: []
-    },
-    {
-      id: 1,
-      name: "livros",
-      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
-      description: "projeto de livros LGBT",
-      progress: 0,
-      components: []
-    },
-    {
-      id: 2,
-      name: "filmesFantasia",
-      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
-      description: "projeto de livros LGBT",
-      progress: 0,
-      components: []
-    },
-    {
-      id: 3,
-      name: "filmesTerror",
-      img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
-      description: "projeto de livros LGBT",
-      progress: 0,
-      components: []
-    }
-  ],
-
-  search: 'filme'
-}
 
 
-function App() {
-
-  return (
-    <div className="App">
-      <h1>Proyecto</h1>
-      <div>
-        <input type="search" id="searchBar" />
-        <button>buscar</button>
-      </div>
-      <br />
-      <div>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      projects: [
         {
-          data.projects.filter(project => project.name.includes(data.search)).map(project => <Project img={project.img} name={project.name} description={project.description} progress={project.progress} />)
+          id: 0,
+          name: "projeto1",
+          img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+          description: "projeto de livros LGBT",
+          progress: 0,
+          components: []
+        },
+        {
+          id: 1,
+          name: "livros",
+          img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+          description: "projeto de livros LGBT",
+          progress: 0,
+          components: []
+        },
+        {
+          id: 2,
+          name: "filmesFantasia",
+          img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+          description: "projeto de livros LGBT",
+          progress: 0,
+          components: []
+        },
+        {
+          id: 3,
+          name: "filmesTerror",
+          img: "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
+          description: "projeto de livros LGBT",
+          progress: 0,
+          components: []
         }
+      ],
 
+      search: ''
+    }
+
+  };
+
+  changeSearch = (e) => {
+    this.setState({ search: e.target.value })
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Proyecto</h1>
+        <div>
+          <input type="search" id="searchBar" value={this.state.search} onChange={this.changeSearch} />
+        </div>
+        <br />
+        <div id="container">
+          {
+            this.state.projects.filter(project => project.name.includes(this.state.search)).map(project => <Project img={project.img} name={project.name} description={project.description} progress={project.progress} />)
+          }
+
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
