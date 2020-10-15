d35da7e428be208ec7d591dcd8357efde1ef83ee
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2020-10-15 17:38:15 -0300

    add GET to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router";
 import "./style/App.css";
 import Header from "./components//Header/Header";
@@ -7,63 +7,20 @@ import Home from "./pages/HomePage/Home";
 import Create from "./pages/CreatePage/Create";
 import Resume from "./pages/ResumePage/Resume";
 
-const projects = [
-  {
-    id: 0,
-    name: "projeto1",
-    img:
-      "https://o4u.com.br/wp-content/uploads/2018/11/gest%C3%A3o-empresarial-na-pr%C3%A1tica.jpg",
-    description: "projeto de livros LGBT",
-    progress: 0,
-    components: [],
-  },
-  {
-    id: 1,
-    name: "livros",
-    img:
-      "https://st.depositphotos.com/1252248/1783/i/450/depositphotos_17830541-stock-photo-open-book-with-colorful-pages.jpg",
-    description: "projeto de livros LGBT",
-    progress: 0,
-    components: [],
-  },
-  {
-    id: 2,
-    name: "filmes de fantasia",
-    img: "https://s.aficionados.com.br/imagens/senhordosaneis.jpg",
-    description: "projeto de livros LGBT",
-    progress: 0,
-    components: [],
-  },
-  {
-    id: 3,
-    name: "filmes de terror",
-    img: "https://cdn.cinepop.com.br/2016/08/filmesdeterror2017-696x353.jpg",
-    description: "projeto de livros LGBT",
-    progress: 0,
-    components: [],
-  },
-  {
-    id: 4,
-    name: "filmes da Marvel",
-    img: "https://miro.medium.com/max/2560/0*qdHImq1G588SB9Ii.jpg",
-    description: "projeto de livros LGBT",
-    progress: 0,
-    components: [],
-  },
-  {
-    id: 5,
-    name: "cursos udemy",
-    img: "https://www.udemy.com/staticx/udemy/images/v6/default-meta-image.png",
-    description: "projeto de livros LGBT",
-    progress: 0,
-    components: [],
-  },
-];
-
-const initialSearch = "";
-
 function App() {
-  const [search, setSearch] = useState(initialSearch);
+  const [search, setSearch] = useState("");
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((response) => setProjects(response));
+  }, []);
 
   return (
     <div className="App">
--- frontend/src/pages/HomePage/Home.js
+++ frontend/src/pages/HomePage/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Home.css";
 import Project from "../../components/Project/Project";
 
