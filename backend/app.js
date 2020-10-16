f9b5f2df07085501e293b1a00849b99101bd4128
douglaslimaxx <douglas.lima@ccc.ufcg.edu.br>
2020-10-16 11:06:42 -0300

    add cors use

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const cors = require("cors")
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
